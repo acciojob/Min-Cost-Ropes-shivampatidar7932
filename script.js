fbb5690d563a5ee7603b3dfae3b444d2eac39ad0
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-20 11:34:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,96 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    extractMin() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+
+        return min;
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[index] < this.heap[parentIndex]) {
+                this.swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    bubbleDown() {
+        let index = 0;
+        while (true) {
+            const leftChild = 2 * index + 1;
+            const rightChild = 2 * index + 2;
+            let smallest = index;
+
+            if (leftChild < this.heap.length && this.heap[leftChild] < this.heap[smallest]) {
+                smallest = leftChild;
+            }
+
+            if (rightChild < this.heap.length && this.heap[rightChild] < this.heap[smallest]) {
+                smallest = rightChild;
+            }
+
+            if (smallest !== index) {
+                this.swap(index, smallest);
+                index = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+
+    swap(i, j) {
+        const temp = this.heap[i];
+        this.heap[i] = this.heap[j];
+        this.heap[j] = temp;
+    }
 }
 
-module.exports=mincost;
+function mincost(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+
+    const minHeap = new MinHeap();
+
+    // Insert all ropes into the min heap
+    for (const rope of arr) {
+        minHeap.insert(rope);
+    }
+
+    let totalCost = 0;
+
+    // Connect ropes until only one rope is left
+    while (minHeap.heap.length > 1) {
+        const rope1 = minHeap.extractMin();
+        const rope2 = minHeap.extractMin();
+        const cost = rope1 + rope2;
+        totalCost += cost;
+        minHeap.insert(cost);
+    }
+
+    return totalCost;
+}
+
+module.exports = mincost;
